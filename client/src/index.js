c27fd4e7910f9a3f1e82b508e9cf026d409ec25e
Anna Pawlik <pawlikanna24@gmail.com>
2021-03-05 11:13:55 +0100

    Add packages

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Root = () => (<App />);
+
+ReactDOM.render(<Root />, document.getElementById('root')); 
