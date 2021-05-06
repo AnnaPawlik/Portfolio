820145ec1ae15e15bb8364d51743abb6b23b1b07
Anna Pawlik <pawlikanna24@gmail.com>
2021-05-06 12:34:55 +0200

    Change section About_me

--- client/src/components/features/About/About.js
+++ client/src/components/features/About/About.js
@@ -17,12 +17,10 @@ class About extends React.Component {
                     <img src={Portfolio} alt="Portfolio" className="image rounded-circle p-4 m-4"/>
                     <div className="intro text-left">
                         <List type="unstyled" className="p-4 ml-4">
-                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Od 10 lat jestem związana z branżą beauty.</li>
-                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> 2 lata temu podjęłam decyzję o zmianie branży i sprawdzeniu się jako Front-End Developer.</li>
+                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Szukam możliwości rozpoczęcia kariery zawodowej jako Junior Front-End Developer.</li>
                             <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Ukończyłam 9-miesięczny bootcamp Web Developer z zakresu front-end i back-end.</li>
-                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Cały czas ćwiczę i rozwijam swoje umiejętności - szczególnie w JavaScript i React.</li>
+                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Znam podstawy HTML, CSS, JS, React pozwalające mi stworzyć podstawowe UI aplikacji.</li>
                             <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Świetnie zarządzam czasem i organizacją pracy.</li>
-                            <li><FontAwesomeIcon icon={faChevronRight} className="arrow" /> Jestem otwarta na nowe technologie.</li>
                         </List>
                     </div>
                 </div>
